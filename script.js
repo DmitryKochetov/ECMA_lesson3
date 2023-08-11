cc5bd1f82dbab2b2a05a325d81bc6ed182f6e7c7
Dmitry <kochetov.dmitry@mail.ru>
2023-08-11 11:15:57 +0300

    task 3

---
+++ script.js
@@ -0,0 +1,158 @@
+// Задание 1: ""Управление персоналом компании""
+
+// Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
+
+// Свойство name (имя) - строка, имя сотрудника.
+// Метод displayInfo() - выводит информацию о сотруднике (имя).
+// Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
+
+// Свойство department (отдел) - строка, отдел, в котором работает менеджер.
+// Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
+// // Пример использования классов
+// const employee = new Employee(""John Smith"");
+// employee.displayInfo();
+// // Вывод:
+// // Name: John Smith
+
+// const manager = new Manager(""Jane Doe"", ""Sales"");
+// manager.displayInfo();
+// // Вывод:
+// // Name: Jane Doe
+// // Department: Sales
+
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+  displayInfo() {
+    console.log(`Name: ${this.name}`);
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, department) {
+    super(name);
+    this.department = department;
+  }
+  displayInfo() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Department: ${this.department}`);
+  }
+}
+
+const employee = new Employee("John Smith");
+employee.displayInfo();
+// Вывод:
+// Name: John Smith
+
+const manager = new Manager("Jane Doe", "Sales");
+manager.displayInfo();
+// Вывод:
+// Name: Jane Doe
+// Department: Sales
+
+// ""Управление списком заказов""
+
+// Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+
+// Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+// Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+// // Пример использования класса
+// class Product {
+// constructor(name, price) {
+// this.name = name;
+// this.price = price;
+// }
+// }
+
+class Order {
+  constructor(orderNumber) {
+    this.orderNumber = orderNumber;
+  }
+  products = [];
+
+  addProduct(product) {
+    this.products.push(product);
+  }
+  getTotalPrice() {
+    return this.products.reduce((sum, current) => sum + current.price, 0);
+  }
+}
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 600
+
+// Задача необязательная для выполнения
+
+// Задача: Создать класс "Студент", который имеет приватные свойства "имя", "возраст" и "средний балл".
+// Класс должен иметь методы для установки и получения значений свойств, а также метод для вывода информации о студенте.
+// Про приватные свойства и методы - https://learn.javascript.ru/private-protected-properties-methods
+
+// const student = new Student();
+// student.setName('Питер Паркер);
+// student.setAge(20);
+// student.setAverageGrade(85);
+// student.displayInfo();
+
+class Student {
+    #name;
+    #age;
+    #averageGrade;
+
+  constructor(name, age, averageGrade) {
+    this.#name = name;
+    this.#age = age;
+    this.#averageGrade = averageGrade;
+  }
+
+  setName(name) {
+    this.#name = name;
+  }
+  setAge(age) {
+    this.#age = age;
+  }
+  setAverageGrade(averageGrade) {
+    this.#averageGrade = averageGrade;
+  }
+  getName() {
+    return this.#name;
+  }
+  getAge() {
+    return this.#age;
+  }
+  getAverageGrade() {
+    return this.#averageGrade;
+  }
+
+
+
+  displayInfo() {
+    console.log(`Name: ${this.#name}`);
+    console.log(`Last Name: ${this.#age}`);
+    console.log(`Average Grade: ${this.#averageGrade}`);
+  }
+
+}
+
+const student = new Student();
+student.setName('Питер Паркер');
+student.setAge(20);
+student.setAverageGrade(85);
+student.displayInfo();
